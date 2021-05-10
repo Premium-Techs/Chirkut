68e9ebdf1931b23bbb27f9fc8c811eefeb0485d7
Rahat <fountaein@yandex.com>
2021-05-10 07:23:20 +0600

    Chat activity done

--- app/src/main/java/com/premiumtechs/chirkut/CustomAdapter.java
+++ app/src/main/java/com/premiumtechs/chirkut/CustomAdapter.java
@@ -1,6 +1,7 @@
 package com.premiumtechs.chirkut;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,20 +17,17 @@ public class CustomAdapter extends ArrayAdapter<Message> {
     private final List<Message> dataSet;
     private final LinearLayout.LayoutParams layoutParamsSelf;
     private final LinearLayout.LayoutParams layoutParamsOthers;
+    private final int CHAT_INSET = 80;
     Context context;
 
     public CustomAdapter(List<Message> messages, @NonNull Context context) {
-        super(context, R.layout.chat_page, messages);
+        super(context, R.layout.activity_chat_page, messages);
         this.dataSet = messages;
         this.context = context;
-        layoutParamsSelf = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        layoutParamsOthers = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        //layoutParamsSelf.setMargins(50, 0, 0, 0);
-        layoutParamsSelf.leftMargin = 50;
-        layoutParamsSelf.topMargin = 0;
-        layoutParamsSelf.rightMargin = 0;
-        layoutParamsSelf.bottomMargin = 0;
-        layoutParamsOthers.setMargins(0, 0, 50, 0);
+        layoutParamsSelf = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParamsOthers = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParamsSelf.setMargins(CHAT_INSET, 0, 0, 0);
+        layoutParamsOthers.setMargins(0, 0, CHAT_INSET, 0);
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -39,7 +37,8 @@ public class CustomAdapter extends ArrayAdapter<Message> {
         if (convertView == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.msg_box_common, parent, false);
-            viewHolder.msgLayout = convertView.findViewById(R.id.msg_box_common_inner);
+            viewHolder.msgLayoutOuter = convertView.findViewById(R.id.msg_box_common_outer);
+            viewHolder.msgLayoutInner = convertView.findViewById(R.id.msg_box_common_inner);
             viewHolder.msgBody = convertView.findViewById(R.id.msg_box_body);
             viewHolder.msgTime = convertView.findViewById(R.id.msg_box_time);
             convertView.setTag(viewHolder);
@@ -47,11 +46,13 @@ public class CustomAdapter extends ArrayAdapter<Message> {
             viewHolder = (ViewHolder) convertView.getTag();
         }
         if (message.getSenderId().equals("0")) {
-            viewHolder.msgLayout.setBackgroundResource(R.drawable.border_msg_self);
-            viewHolder.msgLayout.setLayoutParams(layoutParamsSelf);
+            viewHolder.msgLayoutOuter.setGravity(Gravity.RIGHT);
+            viewHolder.msgLayoutInner.setBackgroundResource(R.drawable.border_msg_self);
+            viewHolder.msgLayoutInner.setLayoutParams(layoutParamsSelf);
         } else {
-            viewHolder.msgLayout.setBackgroundResource(R.drawable.border_msg_others);
-            viewHolder.msgLayout.setLayoutParams(layoutParamsOthers);
+            viewHolder.msgLayoutOuter.setGravity(Gravity.LEFT);
+            viewHolder.msgLayoutInner.setBackgroundResource(R.drawable.border_msg_others);
+            viewHolder.msgLayoutInner.setLayoutParams(layoutParamsOthers);
         }
         viewHolder.msgBody.setText(message.getMessages());
         viewHolder.msgTime.setText(message.getSendTime());
@@ -61,6 +62,6 @@ public class CustomAdapter extends ArrayAdapter<Message> {
 
     private static class ViewHolder {
         TextView msgBody, msgTime;
-        LinearLayout msgLayout;
+        LinearLayout msgLayoutOuter, msgLayoutInner;
     }
 }
